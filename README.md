353c4a6827f376ff8357df4f072c4f7ccf61f82b
Michael Yu <yufeiyohi@outlook.com>
2023-12-10 22:22:21 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,43 @@
+# aaPanel Tools
+
+[English](https://github.com/AaronYES/aaPanel/blob/main/README.md)&nbsp;|&nbsp;[简体中文](https://github.com/AaronYES/aaPanel/blob/main/README_zh.md)
+
+This tool is available for CentOS/Debian/Ubuntu clients for some of your own needs.
+
+## Function
+
+- Install aaPanel on CentOS/Debian/Ubuntu
+- Install BTPanel on CentOS/Debian/Ubuntu
+- Downgrade to aaPanel version 6.8.23
+- Downgrade to BTPanel version 7.7.0 
+- Crack
+- Change aaPanel language to Chinese
+- Delete log files, lock file write permissions
+- Uninstall the aaPanel panel
+- Cleanup script produces junk files
+
+## Usage
+
+~~~
+wget https://raw.githubusercontent.com/AaronYES/aaPanel/main/script/aapanel.sh  -O aapanel.sh && chmod +x aapanel.sh && clear && ./aapanel.sh
+~~~
+
+
+## Changelog
+### June 5, 2022
+- The official website address of the old version of aaPane has been modified, and the download path has been modified to the GitHub repository
+
+### June 3, 2022
+- BTPanel secretly modifies the 7.7.0 version file so that it cannot be downgraded, and the download path is modified to the GitHub repository
+
+### May 30, 2022
+- Modify script path
+
+### May 17, 2022
+- Added pagoda panel downgrade
+
+### May 16, 2022
+- Make Chinese documents
+
+### May 15, 2022
+- Complete the script
---
+++ script/aapanel.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+# By Aaron
+# https://github.com/AaronYES/aaPanel
+
+#彩色
+red(){
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+blue(){
+    echo -e "\033[34m\033[01m$1\033[0m"
+}
+purple(){
+    echo -e "\033[35m\033[01m$1\033[0m"
+}
+
+function English(){
+wget https://raw.githubusercontent.com/yufeiyohi/aaPanel/main/script/aapanel-en.sh  -O aapanel-en.sh && chmod +x aapanel-en.sh && clear && ./aapanel-en.sh
+}
+
+function Chinese(){
+wget https://proxy.mshushu.com.cn/raw.githubusercontent.com/yufeiyohi/aaPanel/main/script/aapanel-zh.sh  -O aapanel-zh.sh && chmod +x aapanel-zh.sh && clear && ./aapanel-zh.sh
+}
+
+# 菜单
+function start_menu(){
+    clear
+    purple " Thank you for using the aaPanel tool."
+    purple " 请选择您的语言｜Please select your language"
+    purple " GitHub：https://github.com/AaronYES/aapanel"
+    yellow " ————————————————————————————————————————————————"
+    green " 1. English"
+    green " 2. 简体中文"
+    yellow " ————————————————————————————————————————————————"
+    green " 0. 退出脚本｜Exit"
+
+    echo
+    read -p "请输入数字｜Please key in numbers：" menuNumberInput
+    case "$menuNumberInput" in
+        1 )
+           English
+	    ;;
+        2 )
+           Chinese
+        ;;
+        0 )
+            exit 1
+        ;;
+        * )
+            clear
+            red "请输入正确数字!｜Please enter the correct number!"
+            start_menu
+        ;;
+    esac
+}
+start_menu "first"
